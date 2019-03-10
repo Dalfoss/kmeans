556cda00419b0e7c3f8f739d600286bd8136c31a
Morten Torgund Dalfoss <mortendalfoss@gmail.com>
2019-03-10 19:54:49 +0100

    Implemented floats to represent centroids and data points

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,7 @@ use pyo3::wrap_pyfunction;
 
 
 #[pyfunction]
-fn kmeans(centroids: Vec<(i64,i64)>, points: Vec<(i64,i64)>) -> PyResult<Vec<(i64,i64)>> {
+fn kmeans(centroids: Vec<(f64,f64)>, points: Vec<(f64,f64)>) -> PyResult<Vec<(f64,f64)>> {
     Ok(move_centroids(&centroids, &points))
 }
 
@@ -17,23 +17,18 @@ fn edist(_py: Python, m: &PyModule) -> PyResult<()> {
 }
 
 
-fn closest_centroids(centroids: &Vec<(i64,i64)>, points: &Vec<(i64,i64)>) -> Vec<usize> {
+fn closest_centroids(centroids: &Vec<(f64,f64)>, points: &Vec<(f64,f64)>) -> Vec<usize> {
     let mut res: Vec<usize> = Vec::with_capacity(points.len());
 
     for n in points {
-        let mut cc: (usize, i64) = (0, std::i64::MAX); // Closest centroid and the squared distance (centroid, dist)
+        let mut cc: (usize, f64) = (0, std::f64::MAX); // Closest centroid and the squared distance (centroid, dist)
 
         for (i,j) in centroids.iter().enumerate() {
-            let sq_dists: (Option<i64>, Option<i64>) = ((n.0-j.0).checked_pow(2), (n.1-j.1).checked_pow(2));
+            let sq_dists: (f64, f64) = ((n.0-j.0).powi(2), (n.1-j.1).powi(2));
             // Matching the squared result to catch integer overflow
-            match sq_dists {
-                (Some(x), Some(y)) => if x+y < cc.1 {
+            if sq_dists.0+sq_dists.1 < cc.1 {
                     cc.0 = i;
-                    cc.1 = x + y
-                }
-                (None,None) => panic!("Interger overflow occured while squaring {} and {}",n.0-j.0, n.1-j.1),
-                (None,Some(_x)) => panic!("Interger overflow occured while squaring {}", n.0-j.0),
-                (Some(_x),None) => panic!("Interger overflow occured while squaring {}", n.1-j.1),
+                    cc.1 = sq_dists.0 + sq_dists.1
             }
         }
         res.push(cc.0);
@@ -41,13 +36,14 @@ fn closest_centroids(centroids: &Vec<(i64,i64)>, points: &Vec<(i64,i64)>) -> Vec
     res
 }
 
-fn move_centroids(centroids: &Vec<(i64,i64)>, points: &Vec<(i64,i64)>) -> Vec<(i64,i64)> {
+fn move_centroids(centroids: &Vec<(f64,f64)>, points: &Vec<(f64,f64)>) -> Vec<(f64,f64)> {
     let p = closest_centroids(&centroids, &points);
-    let mut new_centroids: Vec<(i64,i64)> = Vec::with_capacity(centroids.len());
-    let mut mean_loc: Vec<(i64, (i64,i64))> = vec![(0,(0,0)); centroids.len()];
+    let mut new_centroids: Vec<(f64,f64)> = Vec::with_capacity(centroids.len());
+    let mut mean_loc: Vec<(f64, (f64,f64))> = vec![(0.0,(0.0,0.0)); centroids.len()];
+
     for (i,&j) in p.iter().enumerate() {
         let (x, y) = points[i];
-        mean_loc[j] = (mean_loc[j].0 + 1, ((mean_loc[j].1).0 + x, (mean_loc[j].1).1 + y));
+        mean_loc[j] = (mean_loc[j].0 + 1.0, ((mean_loc[j].1).0 + x, (mean_loc[j].1).1 + y));
     }
     
     for k in mean_loc {
