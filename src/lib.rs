07336755605689359b6615ec3c05197921c96571
Morten Torgund Dalfoss <mortendalfoss@gmail.com>
2019-03-10 14:12:35 +0100

    Implemented running centroids against each point, big improvement

--- src/lib.rs
+++ src/lib.rs
@@ -5,23 +5,26 @@ use pyo3::wrap_pyfunction;
 
 
 #[pyfunction]
-fn euclidean_dist(centroids: Vec<(i64,i64)>, points: Vec<(i64,i64)>) -> PyResult<Vec<Vec<i64>>> {
-    let mut res = Vec::<Vec<i64>>::new();
+fn euclidean_dist(centroids: Vec<(i64,i64)>, points: Vec<(i64,i64)>) -> PyResult<Vec<usize>> {
+    let mut res = Vec::<usize>::new();
 
-    for n in centroids {
-        let mut row = Vec::<i64>::new();
-        for i in &points {
-            let sq_dists: (Option<i64>, Option<i64>) = ((i.0-n.0).checked_pow(2), (i.1-n.1).checked_pow(2));
-            
+    for n in points {
+        let mut cc: (usize, i64) = (0,std::i64::MAX); // Closest centroid and the squared distance (centroid, dist)
+
+        for (i,j) in centroids.iter().enumerate() {
+            let sq_dists: (Option<i64>, Option<i64>) = ((n.0-j.0).checked_pow(2), (n.1-j.1).checked_pow(2));
             // Matching the squared result to catch integer overflow
             match sq_dists {
-                (Some(x), Some(y)) => row.push(x + y),
-                (None,None) => panic!("Interger overflow occured while squaring {} and {}",n.0-i.0, n.1-i.1),
-                (None,Some(_x)) => panic!("Interger overflow occured while squaring {}", n.0-i.0),
-                (Some(_x),None) => panic!("Interger overflow occured while squaring {}", n.1-i.1),
+                (Some(x), Some(y)) => if x+y < cc.1 {
+                    cc.0 = i;
+                    cc.1 = x + y
+                }
+                (None,None) => panic!("Interger overflow occured while squaring {} and {}",n.0-j.0, n.1-j.1),
+                (None,Some(_x)) => panic!("Interger overflow occured while squaring {}", n.0-j.0),
+                (Some(_x),None) => panic!("Interger overflow occured while squaring {}", n.1-j.1),
             }
         }
-        res.push(row)
+        res.push(cc.0)
     }
     
     Ok(res)
@@ -33,3 +36,4 @@ fn edist(_py: Python, m: &PyModule) -> PyResult<()> {
 
     Ok(())
 }
+
