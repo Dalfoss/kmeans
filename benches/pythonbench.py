80d8d1fb28b33d9d94d0a3c3bee93125afa05dfc
Morten Torgund Dalfoss <mortendalfoss@gmail.com>
2019-03-12 22:10:42 +0100

    Benchmark against sklearn kmeans

--- benches/pythonbench.py
+++ benches/pythonbench.py
@@ -1,29 +1,51 @@
 import time
 import sys
 import random
+from sklearn.cluster import KMeans
+import numpy as np
+
 sys.path.append("../target/release/")
 
 import libedist
 
-def run_kmeans():
-    points = []
-    centroid = []
-    for i in range(100):
-        centroid.append((random.randint(1,1000),random.randint(1,1000)))
-    for i in range(1000000):
-        points.append((random.randint(1,1000),random.randint(1,1000)))
 
+
+def run_kmeans(points, centroids):
     start = time.perf_counter()
-    resr = libedist.kmeans(centroid, points)
+    final_centroids = libedist.kmeans(centroids, points)
     end = time.perf_counter()
 
     delta = end - start
     print("Runtime: %f seconds" % delta)
-    sys.stdout.flush()
-    return centroid
+    return final_centroids
 
-def test_my_stuff(benchmark):
-    result = benchmark(run_kmeans)
+
+
+
+def run_sk_kmeans(points, centroids):
+    start = time.perf_counter()
+    kmeans = KMeans(init=centroids, n_init=1, verbose=1, n_clusters=1000).fit(points)
+    end = time.perf_counter()
+    delta = end - start
+    print("Runtime: %f seconds" % delta)
+
+    return kmeans.cluster_centers_
 
 if __name__ == "__main__":
-    run_kmeans()
+
+    # sklean points and centroids
+    sk_points = np.random.randint(1000, size=(100000,2))
+    sk_centroids = sk_points[np.random.choice(len(sk_points)-1, 1000)]
+
+    # libedist points and centroids
+    points = []
+    centroids = []
+
+    for point in sk_points:
+        points.append((point[0], point[1]))
+    for centroid in sk_centroids:
+        centroids.append((centroid[0], centroid[1]))
+    result = run_kmeans(points, centroids)
+    sk_result = run_sk_kmeans(sk_points, sk_centroids)
+    
+#    run_kmeans()
