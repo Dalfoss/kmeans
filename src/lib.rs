710e92acbc782b77f6e1ada12043a29f5fc87a8e
Morten Torgund Dalfoss <mortendalfoss@gmail.com>
2019-03-13 17:10:38 +0100

    Working kmeans++ centroid initialization

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@ extern crate pyo3;
 use std::collections::HashSet;
 use pyo3::prelude::*;
 use pyo3::wrap_pyfunction;
+use rand::prelude::*;
 use rand::distributions::{Distribution, Uniform};
 
 #[pyfunction]
@@ -18,7 +19,7 @@ fn kmeans(points: Vec<(f64,f64)>, k: usize) -> PyResult<Vec<(f64,f64)>> {
         }
         kmeans_inner(new_centroids, points, c_points, iterations)
     }
-    let centroids = init_centroids(&points, k, "random".to_string());
+    let centroids = init_centroids(&points, k, "".to_string());
     let c_points = closest_centroids(&centroids, &points);
     Ok(kmeans_inner(centroids, &points, c_points, 0))
 }
@@ -68,9 +69,48 @@ fn move_centroids(centroids: Vec<(f64,f64)>, points: &Vec<(f64,f64)>, c_points: 
 
 fn init_centroids(points: &Vec<(f64,f64)>, k: usize, method: String) -> Vec<(f64,f64)> {
 
-//    fn kmeans_pp(points: &Vec<(f64, f64)>, k: usize) -> Vec<(f64,f64)> {
-//
-//    }
+    fn kmeans_pp(points: &Vec<(f64, f64)>, k: usize) -> Vec<(f64,f64)> {
+        fn find_next_centroid(dists: &Vec<f64>, rng: &mut ThreadRng) -> usize {
+            let mut best_choice: (usize, f64) = (0, 0.0);
+            for (i, &j) in dists.iter().enumerate() {
+                let value = j * rng.gen::<f64>();
+                if value > best_choice.1 {
+                    best_choice = (i, value);
+                }
+            }
+            best_choice.0
+        }
+        
+        let mut rng = rand::thread_rng();
+        let mut centroids: Vec<(f64,f64)> = Vec::with_capacity(k);
+        let mut min_dist_sq: Vec<f64> = Vec::with_capacity(points.len());
+
+        // Establishes the min_dist_sq vector
+        let first_centroid = points[rng.gen_range(0, points.len()-1)];
+        centroids.push(first_centroid);
+        for &point in points {
+            match centroids.last() {
+                Some((x,y)) => min_dist_sq.push((point.0-x).powi(2) + (point.1-y).powi(2)),
+                None => panic!("fejl i kmeans_pp")
+            }
+        }
+        centroids.push(points[find_next_centroid(&min_dist_sq, &mut rng)]);
+
+        for _n in centroids.len().. k {
+            for (i, &point) in points.iter().enumerate() {
+                if let Some((x,y)) = centroids.last() {
+                    let dist_sq = (point.0-x).powi(2) + (point.1-y).powi(2);
+                    if dist_sq < min_dist_sq[i] {
+                        min_dist_sq[i] = dist_sq;
+                    }
+                } else {
+                    panic!("error in if let kmeans_pp")
+                }
+            }
+            centroids.push(points[find_next_centroid(&min_dist_sq, &mut rng)]);
+        }
+        centroids
+    }
 
     fn random(points: &Vec<(f64, f64)>, k: usize) -> Vec<(f64,f64)> {
         let size = Uniform::from(0..points.len()-1);
@@ -92,9 +132,8 @@ fn init_centroids(points: &Vec<(f64,f64)>, k: usize, method: String) -> Vec<(f64
     if method == "random" {
         random(points, k)
     } else {
-        //        kmeans_pp(points, k)
-        let l: Vec<(f64,f64)> = Vec::new();
-        return l
+        println!("Using kmeans++");
+        kmeans_pp(points, k)
     }
 
 }
