6c099f155cd56b74d0bf6e39808e4d0d7027315e
Morten Torgund Dalfoss <mortendalfoss@gmail.com>
2019-03-18 21:16:22 +0100

    Added slow python implementation of kmeans score in benchmark

--- benches/pythonbench.py
+++ benches/pythonbench.py
@@ -3,7 +3,7 @@ import sys
 import random
 from sklearn.cluster import KMeans
 import numpy as np
-
+import matplotlib.pyplot as plt
 sys.path.append("../target/release/")
 
 import libedist
@@ -39,14 +39,33 @@ def run_sk_kmeans(points, k):
 
     return kmeans.cluster_centers_
 
+def kmeans_score(points, centroids):
+    score = 0
+    for i in centroids:
+        for j in points:
+            score += ((i[0]-j[0])**2 + (i[1]-j[1])**2)/1e6
+    return score
+
 if __name__ == "__main__":
     # sklean points and centroids
-    sk_points = np.random.randint(2000, size=(500000,4))
+    sk_points = np.random.randint(100, size=(50000,2))
 
     # libedist points and centroids
     points = []
     for point in sk_points:
-        points.append([point[0], point[1], point[2], point[3]])
+        points.append([point[0], point[1]])
     result1 = run_kmeans_random(points, 100)
     result2 = run_kmeans(points, 100)
     sk_result = run_sk_kmeans(sk_points, 100)
+
+    result2x = [i[0] for i in result2]
+    result2y = [i[1] for i in result2]
+    skx = [i[0] for i in sk_result]
+    sky = [i[1] for i in sk_result]
+
+    print("libedist score: " + str(kmeans_score(points, result2)))
+    print("sk score: " + str(kmeans_score(sk_points, sk_result)))
+    
+    plt.plot(skx, sky, 'o', color='green')
+    plt.plot(result2x, result2y, 'x', color='red')
+    plt.show()
